40eacd514626ee587a8e7be6fedb869dd2aaa51a
Sveltecult <sveltecult@proton.me>
2023-12-27 11:55:43 +0800

    add sveltekit-mailer

--- README.md
+++ README.md
@@ -1,55 +1,176 @@
-# About
+# SvelteKit Mailer
 
-This repository contains NPM packages I personally maintain. I merge all of them in one repository to streamline and simplify the maintenance process.
+This library allows you to send emails with nice and customizable layout and colors. With expressive syntax and simplifies the creation of common components like buttons, codes, paragraphs and more.
 
-## Packages
+## Installation
 
-Looking for specific package? Here are all available libraries so far:
+To install, use your preferred package manager:
 
-### SvelteKit Router
+```bash
+npm install @sveltecult/sveltekit-mailer
+```
 
-This library facilitates per-route multiple middleware for SvelteKit applications. It simplifies middleware management and execution for different routes within your application.
+## Getting Started
 
-**Package:** [`@sveltecult/sveltekit-router`](https://www.npmjs.com/package/@sveltecult/sveltekit-router)
+To start using this library, you must first set your SMTP credentials to your `.env` file. Currently, only SMTP is supported.
 
-**Source Code:** [`sveltekit-router`](https://github.com/sveltecult/packages/tree/sveltekit-router)
+```env
+MAIL_MAILER=smtp
+MAIL_HOST=
+MAIL_PORT=
+MAIL_USERNAME=
+MAIL_PASSWORD=
+MAIL_FROM_NAME=
+MAIL_FROM_ADDRESS=
+MAIL_SSL=false
+```
 
-### SvelteKit Crypto
 
-Collections of useful cryptographic helper functions designed to simplify common tasks such as hashing passwords, encoding and decoding JSON Web Tokens (JWT) for SvelteKit.
+## Basic Usage
 
-**Package:** [`@sveltecult/sveltekit-crypto`](https://www.npmjs.com/package/@sveltecult/sveltekit-crypto)
+```typescript
+import { Mail } from '@sveltecult/sveltekit-mailer';
 
-**Source Code:** [`sveltekit-crypto`](https://github.com/sveltecult/packages/tree/sveltekit-crypto)
+try {
+    await new Mail({
+        to: 'john@example.com',
+        subject: 'Acme Verification'
+    })
+        .paragraph(
+            'This link and code will only be valid for the next 5 minutes. If the link does not work, you can use the login verification code directly:'
+        )
+        .code('tt226-5398x')
+        .send();
+} catch (e) {
+    console.log(e);
+}
+```
 
-### FormData Validator
+## Customizing Brand
 
-A versatile, framework-agnostic form data validation library with expressive syntax.
+To change the logo, its alt text, height, width, etc, you can modify the `brand.attrs` property on your `Mail`'s second argument.
 
-**Package:** [`@sveltecult/formdata-validator`](https://www.npmjs.com/package/@sveltecult/formdata-validator)
+```typescript
+import { Mail } from '@sveltecult/sveltekit-mailer';
 
-**Source:** [`formdata-validator`](https://github.com/sveltecult/packages/tree/sveltekit-router)
+await new Mail(
+	{
+		to: 'john@example.com'
+	},
+	{
+		brand: {
+			attrs: {
+				src: 'YOUR_LOGO_URL',
+				width: '100',
+				height: '100'
+			}
+		}
+	}
+).send();
+```
 
-**Documentation:** [`https://formdata-validator.sveltecult.com/`](https://formdata-validator.sveltecult.com/)
+You can even customize its CSS:
 
-### Helpers
+```typescript
+import { Mail } from '@sveltecult/sveltekit-mailer';
 
-Collections of useful number and string helper functions designed to simplify common tasks such as pluralization, email obfuscation, random string generation, and more.
+await new Mail(
+	{
+		to: 'john@example.com'
+	},
+	{
+		brand: {
+			attrs: {
+				src: 'YOUR_LOGO_URL',
+				width: '100',
+				height: '100'
+			},
+			css: {
+				background: 'pink'
+			}
+		}
+	}
+).send();
+```
 
-**Package:** [`@sveltecult/helpers`](https://www.npmjs.com/package/@sveltecult/helpers)
+## Disabling Brand
 
-**Source Code:** [`helpers`](https://github.com/sveltecult/packages/tree/helpers)
+By default, every mail will use Svelte's logo. To remove the branding entirely, just set it to `false`.
 
-## Contribution and Feedback
+```typescript
+import { Mail } from '@sveltecult/sveltekit-mailer';
 
-I welcome contributions, suggestions, and feedback from the community. If you find any issues, have ideas for improvements, or wish to contribute, feel free to submit a pull request or open an issue.
+await new Mail(
+    {
+        to: 'john@example.com'
+    },
+    {
+        brand: false
+    }
+).send()
+```
 
-## Get in Touch
+## Changing Colors
 
-You can reach out to me via [`leader@sveltecult.com`](mailto:leader@sveltecult.com) for any inquiries or discussions.
+You can easily change the default colors without changing the layout entirely. We use Tailwind CSS [color palette](https://tailwindcss.com/docs/customizing-colors) as the default.
+
+```typescript
+import { Mail } from '@sveltecult/sveltekit-mailer';
+
+await new Mail(
+    {
+        to: 'john@example.com'
+    },
+    {
+        colors: {
+            body: '#f3f4f6',
+            main: '#ffffff',
+            p: '#0f172a',
+            a: '#3b82f6',
+            hr: '#f3f4f6',
+            action: {
+                background: '#3b82f6',
+                color: '#ffffff',
+                padding: '10px'
+            },
+            code: {
+                background: '#000',
+                color: '#fff',
+                padding: '10px'
+            }
+        }
+    }
+).send();
+```
+
+## Available Functions:
+
+### `.template(template: string)`
+
+Entirely change the default email template.
+
+### `.paragraph(text: string, css: CSSRuleObject = {})`
+
+Adds a paragraph to the email content.
+
+### `.action(href: string, text: string, css: { outer: CSSRuleObject; inner: CSSRuleObject; } = {})`
+
+Adds an action button to the email content.
+
+### `.code(code: string | number, css: { outer: CSSRuleObject; inner: CSSRuleObject; } = {})`
+
+Adds a code block to the email content. Useful for OTP or any unique verification codes.
+
+### `.divider(css: CSSRuleObject = {})`
+
+Adds a line divider to the email content.
+
+### `.send()`
+
+Asynchronously sends the email.
 
 ## License
 
 [![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 
-Everything is licensed under the MIT License.
+This library is licensed under the MIT License.
---
+++ src/lib/index.ts
@@ -0,0 +1,425 @@
+import nodemailer from 'nodemailer';
+import deepmerge from 'deepmerge';
+import {
+	MAIL_FROM_ADDRESS,
+	MAIL_FROM_NAME,
+	MAIL_HOST,
+	MAIL_PASSWORD,
+	MAIL_PORT,
+	MAIL_SSL,
+	MAIL_USERNAME
+} from '$env/static/private';
+import type SMTPTransport from 'nodemailer/lib/smtp-transport/index.js';
+
+interface RecursiveKeyValuePair<K extends keyof any = string, V = string> {
+	[key: string]: V | RecursiveKeyValuePair<K, V>;
+}
+
+type CSSRuleObject = RecursiveKeyValuePair<string, null | string | string[]>;
+
+type MailerOptions = {
+	to: string;
+	from?: string;
+	subject?: string;
+};
+
+type RenderOptions = {
+	brand?:
+		| boolean
+		| Partial<{
+				attrs: { [key: string]: any };
+				css?: CSSRuleObject;
+		  }>;
+	colors?: Partial<{
+		body: string;
+		main: string;
+		p: string;
+		a: string;
+		hr: string;
+		action: {
+			background: string;
+			color: string;
+			padding: string;
+		};
+		code: {
+			background: string;
+			color: string;
+			padding: string;
+		};
+		[key: string]: any;
+	}>;
+};
+
+/**
+ * Represents an Email sender class for creating and sending emails with customizable layouts and content.
+ */
+export class Mail {
+	/**
+	 * The default layout of the email.
+	 */
+	protected layout = `
+		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+		<meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+		<html lang="en">
+
+		<body style="background-color::options.colors.body:;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,Roboto,&quot;Helvetica Neue&quot;,Ubuntu,sans-serif">
+			<table align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0" width="100%" style="max-width:37.5em;background-color::options.colors.main:;margin:0 auto;padding:20px 0 48px;margin-bottom:64px">
+			<tr style="width:100%">
+				<td>
+				<table style="padding:0 48px" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%">
+					<tbody>
+					<tr>
+						<td>
+							:brand:
+							:content:
+						</td>
+					</tr>
+					</tbody>
+				</table>
+				</td>
+			</tr>
+			</table>
+		</body>
+		
+		</html>
+	`;
+
+	/**
+	 * The content of the email.
+	 */
+	protected content: string = '';
+
+	/**
+	 * Options for sending the email.
+	 */
+	protected mailerOptions: MailerOptions;
+
+	/**
+	 * Options to customize the rendering of the email.
+	 */
+	protected renderOptions: RenderOptions = {};
+
+	/**
+	 * Default rendering options.
+	 */
+	protected renderDefaults: RenderOptions = {
+		brand: {
+			attrs: {
+				src: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg',
+				alt: 'Svelte Logo',
+				width: '40',
+				height: '40'
+			},
+			css: {
+				display: 'block',
+				outline: 'none',
+				border: 'none',
+				'text-decoration': 'none'
+			}
+		},
+		colors: {
+			body: '#f3f4f6',
+			main: '#ffffff',
+			p: '#0f172a',
+			a: '#3b82f6',
+			hr: '#f3f4f6',
+			action: {
+				background: '#3b82f6',
+				color: '#ffffff',
+				padding: '10px'
+			},
+			code: {
+				background: '#000',
+				color: '#fff',
+				padding: '10px'
+			}
+		}
+	};
+
+	/**
+	 * Creates an instance of the Mail class.
+	 * @param mailerOptions - Options for sending the email.
+	 * @param renderOptions - Options to customize the rendering of the email.
+	 */
+	constructor(mailerOptions: MailerOptions, renderOptions: RenderOptions = {}) {
+		this.mailerOptions = deepmerge(
+			{
+				from: `"${MAIL_FROM_NAME}" <${MAIL_FROM_ADDRESS}>`
+			},
+			mailerOptions
+		) as {
+			to: string;
+			from?: string;
+			subject?: string;
+		};
+		this.renderOptions = deepmerge(this.renderDefaults, renderOptions);
+	}
+
+	/**
+	 * Sets the email template.
+	 * @param template - The template to be used for the email layout.
+	 */
+	template(template: string) {
+		this.layout = template;
+	}
+
+	/**
+	 * Adds a paragraph to the email content.
+	 * @param text - The text content to be added as a paragraph.
+	 * @param css - CSS rules for styling the paragraph (optional).
+	 * @returns The Mail instance.
+	 */
+	paragraph(
+		text: string,
+		css: CSSRuleObject = {
+			'font-size': '16px',
+			lineHeight: '24px',
+			margin: '16px 0',
+			color: this.renderOptions.colors.p,
+			'text-align': 'left'
+		}
+	) {
+		let content = `<p style="${this.inliner(css)}">${text}</p>`.replace(
+			/<a\s+([^>]*)>/gi,
+			(match, p1) => {
+				if (!/style="[^"]*"/.test(match)) {
+					return `<a ${p1} style="color:${this.renderOptions.colors.a};text-decoration:none">`;
+				} else {
+					return match;
+				}
+			}
+		);
+
+		this.content += content;
+
+		return this;
+	}
+
+	/**
+	 * Adds an action button to the email content.
+	 * @param href - The URL the action button points to.
+	 * @param text - The text content of the action button.
+	 * @param css - CSS rules for styling the action button (optional).
+	 * @returns The Mail instance.
+	 */
+	action(
+		href: string,
+		text: string,
+		css: {
+			outer: CSSRuleObject;
+			inner: CSSRuleObject;
+		} = {
+			outer: {
+				'background-color': this.renderOptions.colors.action.background,
+				'border-radius': '5px',
+				color: this.renderOptions.colors.action.color,
+				'font-size': '16px',
+				'font-weight': 'bold',
+				'text-decoration': 'none',
+				'text-align': 'center',
+				display: 'inline-block',
+				'line-height': '100%'
+			},
+			inner: {
+				'background-color': this.renderOptions.colors.action.background,
+				'border-radius': '5px',
+				color: this.renderOptions.colors.action.color,
+				'font-size': '16px',
+				'font-weight': 'bold',
+				'text-decoration': 'none',
+				'text-align': 'center',
+				display: 'inline-block',
+				width: '100%',
+				'max-width': '100%',
+				'line-height': '120%',
+				'text-transform': 'none',
+				'mso-padding-alt': '0px',
+				'mso-text-raise': '7.5px',
+				padding: '10px'
+			}
+		}
+	): this {
+		this.content += `
+			<a
+				href="${href}"
+				target="_blank"
+				style="${this.inliner(css.outer)}"
+			>
+				<span>
+					<!--[if mso]><i style="letter-spacing: 10px;mso-font-width:-100%;mso-text-raise:15" hidden>&nbsp;</i><![endif]-->
+				</span>
+				<span style="${this.inliner(css.inner)}">
+					${text}
+				</span>
+				<span>
+					<!--[if mso]><i style="letter-spacing: 10px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]-->
+				</span>
+			</a>
+		`;
+
+		return this;
+	}
+
+	/**
+	 * Adds a code block to the email content.
+	 * @param code - The code or number to be displayed.
+	 * @param css - CSS rules for styling the code block (optional).
+	 * @returns The Mail instance.
+	 */
+	code(
+		code: string | number,
+		css: {
+			outer: CSSRuleObject;
+			inner: CSSRuleObject;
+		} = {
+			outer: {
+				background: '#f1f5f9',
+				'border-radius': '5px',
+				'margin-right': '0px',
+				'margin-bottom': '30px',
+				padding: '43px 23px'
+			},
+			inner: {
+				color: '#111827',
+				'font-family': 'monospace',
+				'font-weight': '700',
+				'font-size': '30px',
+				'line-height': '24px',
+				margin: '16px 0',
+				'text-align': 'center',
+				'vertical-align': 'middle'
+			}
+		}
+	): this {
+		this.content += `
+			<table
+				style="${this.inliner(css.outer)}"
+				align="center"
+				border="0"
+				cellPadding="0"
+				cellSpacing="0"
+				role="presentation"
+				width="100%"
+			>
+				<tbody>
+					<tr>
+						<td>
+							<p style="${this.inliner(css.inner)}">${code}</p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+	
+		`;
+
+		return this;
+	}
+
+	/**
+	 * Adds a line divider to the email content.
+	 * @param css - CSS rules for styling the line divider (optional).
+	 * @returns The Mail instance.
+	 */
+	divider(
+		css: CSSRuleObject = {
+			width: '100%',
+			border: 'none',
+			'border-top': `1px solid ${this.renderOptions.colors.hr}`,
+			'border-color': this.renderOptions.colors.hr,
+			margin: '20px 0'
+		}
+	): this {
+		this.content += `<hr style="${this.inliner(css)}" />`;
+
+		return this;
+	}
+
+	/**
+	 * Converts an object to HTML attributes.
+	 * @param attributes - Object containing attributes and their values.
+	 * @returns HTML attributes as a string.
+	 * @private
+	 */
+	private attrs(attributes: { [key: string]: string }): string {
+		return Object.keys(attributes)
+			.map((key) => {
+				const kebabCaseKey = key.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
+				const value = attributes[key];
+
+				return `${kebabCaseKey}="${value}"`;
+			})
+			.join(' ');
+	}
+
+	/**
+	 * Converts CSS rules to inline CSS.
+	 * @param css - CSS rules to be converted to inline CSS.
+	 * @returns Inline CSS as a string.
+	 * @private
+	 */
+	private inliner(css: CSSRuleObject) {
+		let styles = '';
+
+		const toKebabCase = (str: string) => {
+			return str.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
+		};
+
+		for (const property in css) {
+			if (Object.hasOwnProperty.call(css, property)) {
+				const kebabCaseProperty = toKebabCase(property);
+				styles += `${kebabCaseProperty}:${css[property]};`;
+			}
+		}
+
+		return styles.trim();
+	}
+
+	/**
+	 * Builds the final email content by replacing placeholders with actual content.
+	 * @private
+	 */
+	private build() {
+		if (typeof this.renderOptions.brand === 'object') {
+			this.layout = this.layout.replace(
+				':brand:',
+				`<img ${this.attrs(this.renderOptions.brand.attrs)} style="${this.inliner(
+					this.renderOptions.brand.css
+				)}" /><hr style="width:100%;border:none;border-top:1px solid ${
+					this.renderOptions.colors.hr
+				};border-color:${this.renderOptions.colors.hr};margin:20px 0;">`
+			);
+		} else {
+			this.layout = this.layout.replace(':brand:', '');
+		}
+
+		return this.layout
+			.replace(':options.colors.body:', this.renderOptions.colors.body)
+			.replace(':options.colors.main:', this.renderOptions.colors.main)
+			.replace(':content:', this.content);
+	}
+
+	/**
+	 * Asynchronously sends the email.
+	 * @returns A promise indicating the success or failure of sending the email.
+	 */
+	async send(): Promise<SMTPTransport.SentMessageInfo> {
+		const html = this.build();
+
+		const transporter = nodemailer.createTransport({
+			host: MAIL_HOST,
+			port: Number(MAIL_PORT) || 0,
+			secure: MAIL_SSL === 'true',
+			auth: {
+				user: MAIL_USERNAME,
+				pass: MAIL_PASSWORD
+			}
+		});
+
+		return await transporter.sendMail({
+			from: this.mailerOptions.from,
+			to: this.mailerOptions.to,
+			subject: this.mailerOptions.subject,
+			html: html
+		});
+	}
+}
